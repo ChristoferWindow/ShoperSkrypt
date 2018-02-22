6ebda6df5ad916b76b92ae6a94aebde71ac0d587
WINI <imbatman@wp.pl>
2018-02-22 19:30:45 +0100

    Not working

--- sizes.js
+++ sizes.js
@@ -1,6 +1,8 @@
 
 <script>
 $(document).ready(function() {
+  if(document.getElementsByClassName('product_th_cus'))
+  {
       console.log("Start indexing");
       function index_sizes(){
       console.log('inside index');
@@ -8,15 +10,22 @@ $(document).ready(function() {
       var table;
       table = document.getElementsByClassName('product_th_cus');
 
-      console.log(table[0].rows[1].innerHTML+"table test rows");
+      console.log(table[0].rows.length+"table rows length");
 
-      for (i = 0 , row= table[0].rows[i]; i< table[0].rows.length; i++) {
-        console.log(row.innerHTML+":row  test");
+      for ( var i = 0 ; i< table[0].rows.length; i++) {
+        console.log("first loop:"+i);
+
+        console.log(table[0].rows[i].innerHTML+":row  test:"+i);
            //iterate through rows
            //rows would be accessed using the "row" variable assigned in the for loop
-           for (var j = 0, col= row.cells[j]; j < row.cells.length; j++) {
-             let text = col.innerHTML;
-             console.log("text  test :"+text);
+           for (var j = 0; j < row.cells.length-1; j+=1) {
+             var row = table[0].rows[j]
+             var col = row.cells[j];
+             var cells= row.cells.length;
+             var col2 = row.cells[j+1];
+             console.log(cells+":cells length :"+j);
+             console.log(text+":col test");
+             console.log(text2+":col 2 test");
             if(col.innerHTML.search("Rozmiar"))
              {
                console.log("Mamy Rozmiar")
@@ -24,10 +33,11 @@ $(document).ready(function() {
 
              //iterate through columns
              //columns would be accessed using the "col" variable assigned in the for loop
-           }
-         }
+          }
+      }
         return;
       }
       index_sizes();
+    }
 });
 </script>
