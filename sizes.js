33948a13371f999ff3377a3a42de124fb5062089
WINI <imbatman@wp.pl>
2018-02-22 19:31:09 +0100

    Back to basics

--- sizes.js
+++ sizes.js
@@ -12,28 +12,17 @@ $(document).ready(function() {
 
       console.log(table[0].rows.length+"table rows length");
 
-      for ( var i = 0 ; i< table[0].rows.length; i++) {
-        console.log("first loop:"+i);
-
-        console.log(table[0].rows[i].innerHTML+":row  test:"+i);
-           //iterate through rows
-           //rows would be accessed using the "row" variable assigned in the for loop
-           for (var j = 0; j < row.cells.length-1; j+=1) {
-             var row = table[0].rows[j]
-             var col = row.cells[j];
-             var cells= row.cells.length;
-             var col2 = row.cells[j+1];
-             console.log(cells+":cells length :"+j);
-             console.log(text+":col test");
-             console.log(text2+":col 2 test");
-            if(col.innerHTML.search("Rozmiar"))
-             {
-               console.log("Mamy Rozmiar")
-             }
-
-             //iterate through columns
-             //columns would be accessed using the "col" variable assigned in the for loop
-          }
+      for (var i = 0, row; row = table[0].rows[i]; i++) {
+        console.log("iterator:"+i);
+         console.log("row:"+row.innerHTML);
+         //iterate through rows
+         //rows would be accessed using the "row" variable assigned in the for loop
+         for (var j = 0, col; col = row.cells[j]; j++) {
+           console.log("iterator:"+j);
+           console.log("col:"+col.innerHTML);
+           //iterate through columns
+           //columns would be accessed using the "col" variable assigned in the for loop
+         }
       }
         return;
       }
